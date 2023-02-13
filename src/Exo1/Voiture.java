1f0038d8e17aaeb303f0916e547894c9e00e2368
MAKAMTE MEFFEJA LEANDRA GAELLE <58870767+Mleandra@users.noreply.github.com>
2023-02-13 11:06:26 +0100

    enoncé

--- src/Exo1/Voiture.java
+++ src/Exo1/Voiture.java
@@ -1,11 +1,19 @@
 package Exo1;
 
+/*
+* Creer une classe voiture avec les attributs Marques , Modeles  ,date  d' immatriculations ,  vitesse max
+* Ajouter le constructeur  pouvant faire l'initialisations
+*Ajouter  Nombres de voiture cree
+* afficher le nombre de voiture cree grace  a nombre de voiture creer
+*
+* */
+
 import java.time.LocalDate;
 
 public class Voiture {
     String marque;
     String modele;
-    LocalDate dateImatriculaions;
+    LocalDate dateImmatriculaions;
     int vitesseMax;
     static int nombreVoituresCree = 0; // nombres  de voiture crée
     
@@ -13,14 +21,14 @@ public class Voiture {
         System.out.println("Creation d'une voiture ...");
         this.marque = marque ;
         this.modele =modele ;
-        this.dateImatriculaions =dI;
+        this.dateImmatriculaions =dI;
         this.vitesseMax = vMax;
         nombreVoituresCree++ ;   }
 
     public void infos(){ // affichage des informations d'une voiture
         System.out.println("Marque -> "+this.marque);
         System.out.println("Modele -> "+this.modele);
-        System.out.println("Date d'imatriculation -> "+this.dateImatriculaions);
+        System.out.println("Date d'immatriculation -> "+this.dateImmatriculaions);
         System.out.println("Vitesse Max -> "+this.vitesseMax);
     }
 }
