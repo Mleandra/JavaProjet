1b372a55cc3c56c1aa4b310d805d4bfe552abb71
MAKAMTE MEFFEJA LEANDRA GAELLE <58870767+Mleandra@users.noreply.github.com>
2023-02-13 09:17:51 +0100

    second

--- src/Day1/Application.java
+++ src/Day1/Application.java
@@ -3,5 +3,7 @@ package Day1;
 public class Application {
     public static void main(String[] args){
         System.out.println("hello lind ");
+        System.out.println("on fait un petit recylage en java");
     }
+
 }
