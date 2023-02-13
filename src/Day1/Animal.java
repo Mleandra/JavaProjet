8f1da27bd5796db3e54e22fe76a1cfaca1a98d05
MAKAMTE MEFFEJA LEANDRA GAELLE <58870767+Mleandra@users.noreply.github.com>
2023-02-13 09:56:30 +0100

    constructeur et creation d'exemple

--- src/Day1/Animal.java
+++ src/Day1/Animal.java
@@ -1,5 +1,27 @@
 package Day1;
 
+import java.time.LocalDate;
+
 public class Animal {
+// attributs d'instance
+    String nom;
+    int age;
+    LocalDate dateNaissance;
+    // attributs de classe :
+    static int  AgeMax = 50;
+    public  Animal(){ // constructeur sans paramétre de la classe  naimal
+        System.out.println("Constructeur d'un animal");
+    }
+    public  Animal(String nom ,int age, LocalDate naissance){ // constructeur sans paramétre de la classe  naimal
+        System.out.println("Constructeur  avec paramétre");
+        this.nom = nom;
+        this.age = age;
+        this.dateNaissance = naissance;
+
+
+    }
+
+
+
 
 }
--- src/Day1/Application.java
+++ src/Day1/Application.java
@@ -1,9 +1,32 @@
 package Day1;
 
+import java.time.LocalDate;
+
 public class Application {
     public static void main(String[] args){
-        System.out.println("hello lind ");
-        System.out.println("on fait un petit recylage en java");
+        //System.out.println("hello lind ");
+        //System.out.println("on fait un petit recylage en java");
+
+
+        // 1) Création d'une instance
+
+        Animal a1 = new Animal(); // appel au contructeur par défaut
+
+        Animal a2 = new Animal(); // appel au contructeur par défaut
+
+        System.out.println(a1.nom);  //null
+        System.out.println(a1.age);  // 0
+        LocalDate naiss = LocalDate.of(23,02,11);
+        Animal a3 = new Animal("Rex", 3, naiss);
+        System.out.println("Nom : " + a3.nom);
+        System.out.println("Age : " + a3.age);
+        System.out.println("Date de naissance: " + a3.dateNaissance);
+
+        System.out.println(Animal.AgeMax);
+
+        //System.out.println(a1.AgeMax);
+        //System.out.println(a2.AgeMax);
+
     }
 
 }
